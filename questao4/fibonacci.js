fb5173950fdabc5700fc7224882d4dd5067bb68f
tavaresmirako <tavaresmirako23@gmail.com>
2022-02-04 14:22:38 -0300

    i

---
+++ questao4/fibonacci.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    var n = window.prompt("Escolha um número");
+
+
+    fib(n);
+
+    function fib(n) {
+        var fibonacci = [];
+        fibonacci[0] = 0;
+        fibonacci[1] = 1;
+        for (var i = 2; i <= n; i++) {
+            fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+        }
+
+
+        window.alert("O fibonacci desse número é " + fibonacci);
+    }
+
+});
